6f2a8df6d8d506693da1d4f4add20bc68693538f
George Ma <z.george.ma@gmail.com>
2018-09-28 12:10:02 +1000

    Improved retry interface

---
+++ ReduxPattern/Task.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReduxPattern
+{
+    public class RetryPolicy
+    {
+        private readonly Func<bool> shouldRetry;
+        private readonly Action incrementRetryCount;
+        private readonly Func<Task> delay;
+
+        public RetryPolicy(Func<bool> shouldRetry, Action incrementRetryCount, Func<Task> delay)
+        {
+            this.shouldRetry = shouldRetry;
+            this.incrementRetryCount = incrementRetryCount;
+            this.delay = delay;
+        }
+
+        public bool ShouldRetry => shouldRetry();
+        public void IncrementRetryCount() => incrementRetryCount();
+        public Task Delay => delay();
+
+        public static RetryPolicy Immediate(int maxRetryCount)
+        {
+            int retryCount = 0;
+            return new RetryPolicy(
+                shouldRetry: () => retryCount < maxRetryCount,
+                incrementRetryCount: () => retryCount++,
+                delay: () => Task.FromResult(true)
+            );
+        }
+
+        public static RetryPolicy Constant(int millisecondsBackoff, int maxRetryCount)
+        {
+            int retryCount = 0;
+            return new RetryPolicy(
+                shouldRetry: () => retryCount < maxRetryCount,
+                incrementRetryCount: () => retryCount++,
+                delay: () => Task.Delay(millisecondsBackoff)
+            );
+        }
+
+        public static RetryPolicy Random(int millisecondsBackoffMin, int millisecondsBackoffMax, int maxRetryCount)
+        {
+            var random = new Random();
+
+            int retryCount = 0;
+            return new RetryPolicy(
+                shouldRetry: () => retryCount < maxRetryCount,
+                incrementRetryCount: () => retryCount++,
+                delay: () => Task.Delay(random.Next(millisecondsBackoffMin, millisecondsBackoffMax))
+            );
+        }
+    }
+
+    public class RetryContext<T>
+    {
+        internal Func<Task<T>> Execute { get; set; }
+        internal Task<T> Value { get; set; }
+        internal bool ContinueOnCapturedContext { get; set; }
+    }
+}
+
+namespace ReduxPattern.TaskExtension
+{
+    public static class Task
+    {
+        public static System.Threading.Tasks.Task Create<T>(Func<CancellationToken, Task<T>> createProc, TimeSpan timeout)
+        {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(timeout);
+
+            return createProc(cts.Token);
+        }
+
+        public static RetryContext<T> Retry<T>(Func<Task<T>> retryProc, bool continueOnCapturedContext = false) =>
+            new RetryContext<T>
+            {
+                Execute = retryProc,
+                Value = retryProc(),
+                ContinueOnCapturedContext = continueOnCapturedContext
+            };
+
+        public static RetryContext<T> When<T, TException>(this RetryContext<T> self, RetryPolicy retryPolicy)
+            where TException : Exception
+        {
+            self.Value = self.Value.Retry<TException, T>(retryPolicy, self.Execute, self.ContinueOnCapturedContext);
+            return self;
+        }
+
+        public static Task<T> Value<T>(this RetryContext<T> self) => self.Value;
+
+        private static async Task<T> Retry<TException, T>(this Task<T> self, RetryPolicy retryPolicy, Func<Task<T>> retryProc, bool continueOnCapturedContext)
+            where TException : Exception
+        {
+            TException ex;
+            do
+            {
+                try
+                {
+                    return await self.ConfigureAwait(continueOnCapturedContext);
+                }
+                catch (TException e)
+                {
+                    ex = e;
+
+                    await retryPolicy.Delay.ConfigureAwait(continueOnCapturedContext);
+
+                    self = retryProc();
+                    retryPolicy.IncrementRetryCount();
+                }
+            }
+            while (retryPolicy.ShouldRetry);
+
+            throw ex;
+        }
+    }
+}
--- ReduxPattern/TaskExtension.cs
+++ ReduxPattern/TaskExtension.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿using ReduxPattern;
+using System;
 using System.Threading.Tasks;
 
-namespace ReduxPattern
+namespace System.Threading.Tasks
 {
-    public class RetryContext<T>
-    {
-        public bool Break = false;
-        public bool ContinueOnCapturedContext = false;
-        public int ErrorCount = 0;
-        public T Value { get; set; }
-    }
-
-    public static class Tasks
+    public static partial class TaskExtension
     {
         public static async Task<TResult> Then<TResult>(this Task self, Func<Task<TResult>> thenProc, bool continueOnCapturedContext = false)
         {
@@ -55,33 +45,5 @@ namespace ReduxPattern
         public static Task<T> Catch<TException, T>(this Task<T> self, Func<TException, T> catchProc, bool continueOnCapturedContext = false)
             where TException : Exception =>
             self.Catch((TException e) => Task.FromResult(catchProc(e)), continueOnCapturedContext);
-
-        public static async Task<T> Retry<TException, TContext, T>(this Task<T> self, RetryContext<TContext> retryContext, Func<TException, RetryContext<TContext>, Task<T>> retryProc)
-            where TException : Exception
-        {
-            do
-            {
-                try
-                {
-                    return await self.ConfigureAwait(retryContext.ContinueOnCapturedContext);
-                }
-                catch (TException e)
-                {
-                    retryContext.ErrorCount++;
-                    self = retryProc(e, retryContext);
-                }
-            }
-            while (!retryContext.Break);
-
-            return await self.ConfigureAwait(retryContext.ContinueOnCapturedContext);
-        }
-
-        public static Task Create<T>(Func<CancellationToken, Task<T>> createProc, TimeSpan timeout)
-        {
-            var cts = new CancellationTokenSource();
-            cts.CancelAfter(timeout);
-
-            return createProc(cts.Token);
-        }
     }
 }
